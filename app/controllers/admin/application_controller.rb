6078d1bce804f8c99d01c62f79164884b375ebe6
entersoft <apps@entersoft.in>
2017-11-10 19:54:18 +0530

    admin dashboard added

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,8 +6,9 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
     before_action :authenticate_user!
+    before_action :authenticate_admin
+    
 
     def authenticate_admin
       redirect_to(root_path) unless current_user.email == "khalidhmhd@gmail.com"
