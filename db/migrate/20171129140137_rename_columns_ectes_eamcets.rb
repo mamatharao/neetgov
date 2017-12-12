aeb6f793c654fb9f771e32ebf8463f6abbe60784
mamatharao <mamatha@entersoft.in>
2017-12-12 16:59:13 +0000

    Update 20171129140137_rename_columns_ectes_eamcets.rb

--- db/migrate/20171129140137_rename_columns_ectes_eamcets.rb
+++ db/migrate/20171129140137_rename_columns_ectes_eamcets.rb
@@ -4,6 +4,7 @@ class RenameColumnsEctesEamcets < ActiveRecord::Migration[5.1]
   	rename_column :ecets, :type, :imageyes
   	rename_column :ecets, :que_image, :questionimage
   	
+
   	rename_column :eamcets, :test_count, :testcount
   	rename_column :eamcets, :image_yes, :imageyes
   	rename_column :eamcets, :image_question, :questionimage
